06959ed07c73c79fb9166dd689e55c82352dd2eb
AEToluca <aeestrada1004@gmail.com>
2026-03-04 14:53:41 -0500

    Removed old unecessary files and established connection to db

--- web/scripts/main.js
+++
@@ -1,7 +0,0 @@
-// Read stored values and show them in the header. If missing, redirect to login.
-(function() {
-  const name = localStorage.getItem('username');
-  const id = localStorage.getItem('userid');
-  const el = document.getElementById('logged-in');
-  el.textContent = `Logged In As: ${name} (${id})`;
-})();
